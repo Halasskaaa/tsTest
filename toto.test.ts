73783305f8ed6deae9d101a4ac8326d7b0fa8f00
Halasskaaa <phanthilinhtra25@gmail.com>
2026-03-13 20:43:23 +0100

    Fix test cases in toto.test.ts

--- toto.test.ts
+++ toto.test.ts
@@ -5,15 +5,24 @@ describe('Toto', () => {
     test('Egy elem hozzáadása nem dob kivételt', () => {
         const toto = new Toto();
         expect(() => toto.ujEredmeny("Csapat1", 3, "Csapat2", 4)).not.toThrow();
-    })
+    });
 
     test('15. elem hozzáadása kivételt dob', () => {
         const toto = new Toto();
 
-        for (let i = 0; i < 14 i++) {
+        for (let i = 0; i < 14; i++) {
             toto.ujEredmeny("A", 1, "B", 2);
         }
-
         expect(() => toto.ujEredmeny("C", 1, "D", 2)).toThrow();
+    });
+
+    test('Üres csapatnév kivételt dob', () => {
+        const toto = new Toto();
+        expect(() => toto.ujEredmeny("", 1, "B", 2).toThrow();
+    });
+
+    test('Negatív gól kivételt dob', () => {
+        const toto = new Toto();
+        expect(() => toto.ujEredmeny("A", -1, "B", 2)).toThrow();
     });
 });
